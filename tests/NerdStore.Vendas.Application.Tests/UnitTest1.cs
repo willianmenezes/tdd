e0f4412c550e47be262de613b5ede5fa7c9b0c1d
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-10-11 15:38:09 -0300

    Iniciada a aplicacao com projetos de testes e classlib

---
+++ tests/NerdStore.Vendas.Application.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/NerdStore.Vendas.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
