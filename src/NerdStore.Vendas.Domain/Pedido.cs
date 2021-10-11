2f8206f16b04652724824cbbb5cef740ee359d53
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-10-11 16:09:33 -0300

    Criado componenetes iniciais para o primeiro caso de teste

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public decimal ValorTotal { get; private set; }
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItems.Add(pedidoItem);
+            ValorTotal = PedidoItems.Sum(x => x.ValorUnitario * x.Quantidade);
+        }
+
+    }
+
+    public class PedidoItem
+    {
+        public PedidoItem(Guid produtoId, string nomeProduto, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            NomeProduto = nomeProduto;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        public Guid ProdutoId { get; private set; }
+        public string NomeProduto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+    }
+}
---
+++ tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public  class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar item nove pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arranje
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto teste", 2, 100);
+
+            // act
+            pedido.AdicionarItem(pedidoItem);
+
+            // assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+    }
+}
